b978a688311cc197adb27c68a2e7dcb537060ba3
Danielambrose-web <danielessientech121@gmail.com>
2026-05-13 11:53:16 +0100

    basic css of the header and routing

--- Food-Website/src/main.jsx
+++ Food-Website/src/main.jsx
@@ -2,8 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-dom'
-//  'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
