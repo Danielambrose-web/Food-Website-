e53b4fe2c7ba3049335a5f8ef34337fa7491a8f8
Danielambrose-web <danielessientech121@gmail.com>
2026-05-13 11:30:01 +0100

    essential

---
+++ Food-Website/src/App.jsx
@@ -0,0 +1,21 @@
+
+import Navbar from "./components/Navbar/Navbar"
+import {Routes, Route} from 'react-dom'
+import Home from './pages/Home/Home.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
+
+const App = () => {
+  return (
+    <div className="app">
+     <Navbar/>
+     <Routes>
+      <Route path='/' element ={<Home/>}/>
+      <Route path='/Cart' element ={<Cart/>}/>
+      <Route path ='./PlaceOrder' element = {<PlaceOrder/>}/>
+     </Routes>
+    </div>
+  )
+}
+
+export default App
