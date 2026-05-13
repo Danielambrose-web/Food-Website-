89c991aefe49776f1802687f592408dfc41f07a2
Danielambrose-web <danielessientech121@gmail.com>
2026-05-13 11:52:49 +0100

    instal router domand make therouting work

--- Food-Website/src/App.jsx
+++ Food-Website/src/App.jsx
@@ -1,6 +1,6 @@
 
 import Navbar from "./components/Navbar/Navbar"
-import {Routes, Route} from 'react-dom'
+import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
