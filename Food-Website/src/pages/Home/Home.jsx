c5251856236acc4ac1382d93e1f442f60ed77257
Danielambrose-web <danielessientech121@gmail.com>
2026-05-13 22:58:24 +0100

    Mount the explore menu component and set usestate for menus animation

--- Food-Website/src/pages/Home/Home.jsx
+++ Food-Website/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
-import "./Home.css"
-import  Header from '../../components/Header/Header'
+import "./Home.css";
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import { useState } from "react";
 
 const Home = () => {
+  const [catergory, setCatergory] = useState("All");
   return (
     <div>
       <Header />
+      <ExploreMenu catergory={catergory} setCatergory={setCatergory} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
