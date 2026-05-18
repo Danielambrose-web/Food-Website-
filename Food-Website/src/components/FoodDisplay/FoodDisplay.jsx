bf090914f0eddc4ce0d3c28f13d5497fc7a7c196
Danielambrose-web <danielessientech121@gmail.com>
2026-05-18 10:05:58 +0100

    food display component using mapping displays items and food display item

---
+++ Food-Website/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
+
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dishes for you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItem
+              key={index}
+              name={item.name}
+              id={item.id}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FoodDisplay;
---
+++ Food-Website/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,22 @@
+import { assets     } from "../../assets/assets";
+import "./FoodItem.css";
+
+const FoodItem = ({ id, description, name, price, image }) => {
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-img" src={image} alt="" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-rating">
+          {name}
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
