9720c85ebd3266eca9dc645d5fb3e1655c9c879a
Danielambrose-web <danielessientech121@gmail.com>
2026-05-18 10:07:12 +0100

    mount food display on home page

--- Food-Website/src/pages/Cart/Cart.jsx
+++ Food-Website/src/pages/Cart/Cart.jsx
@@ -1,9 +1,11 @@
+// import Header from '../../components/Header/Header'
 import './Cart.css'
 
 const Cart = () => {
   return (
     <div>
-      
+       {/* <Header /> */}
+
     </div>
   )
 }
--- Food-Website/src/pages/Home/Home.jsx
+++ Food-Website/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import { useState } from "react";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 
 const Home = () => {
   const [catergory, setCatergory] = useState("All");
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu catergory={catergory} setCatergory={setCatergory} />
+      <FoodDisplay category={catergory}/>
     </div>
   );
 };
