9ce6543a5585f7ac3c7e2a351958397232cd82b0
Danielambrose-web <danielessientech121@gmail.com>
2026-05-21 19:50:07 +0100

    added function for add to cart and remove from cart to save context

--- Food-Website/src/context/StoreContext.jsx
+++ Food-Website/src/context/StoreContext.jsx
@@ -1,10 +1,34 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const contextValue = {food_list};
+  const [cartItem, setcartItem] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItem[itemId]) {
+      setcartItem((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setcartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setcartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(()=>{
+    console.log(cartItem)
+  },[cartItem])
+
+  const contextValue = {
+    food_list,
+    cartItem,
+    setcartItem,
+    addToCart,
+    removeFromCart,
+  };
 
   return (
     <StoreContext.Provider value={contextValue}>
